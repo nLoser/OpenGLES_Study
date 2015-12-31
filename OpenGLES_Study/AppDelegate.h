b1c10ccd873584fafe10807feb75270dd98dfd42
Ross <954777574@qq.com>
2015-12-31 14:20:45 +0800

    Initial Commit

---
+++ OpenGLES_Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES_Study
+//
+//  Created by LV on 15/12/31.
+//  Copyright © 2015年 Wieye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLES_Study/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLES_Study
+//
+//  Created by LV on 15/12/31.
+//  Copyright © 2015年 Wieye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
